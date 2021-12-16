473637e9a2a75aafa4908f9c9db8e1f540ec1664
IbrahemAtef <bulbul.20001@gmail.com>
2021-12-16 18:49:25 +0200

    admin dashboard v1

---
+++ client/src/layouts/tables/index.js
@@ -0,0 +1,120 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import teachersTableData from "layouts/tables/data/teachersTableData";
+import MDButton from "components/MDButton";
+// import projectsTableData from "layouts/tables/data/projectsTableData";
+import { useMaterialUIController } from "context";
+
+function Tables() {
+  const { columns, rows } = teachersTableData();
+  // const { columns: pColumns, rows: pRows } = projectsTableData();
+  const [controller] = useMaterialUIController();
+  const { sidenavColor } = controller;
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Teachers Table
+                </MDTypography>
+              </MDBox>
+
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+          {/* <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Projects Table
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns: pColumns, rows: pRows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid> */}
+        </Grid>
+      </MDBox>
+      <MDBox p={2} mt="auto">
+        <MDButton
+          component="a"
+          href="" // add new teacher
+          rel="noreferrer"
+          variant="gradient"
+          color={sidenavColor}
+        >
+          Add New Teacher
+        </MDButton>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Tables;
